ca2d6d18fee0a18ebb751403a821718fc6fe9c2d
matskl <matskl@stud.ntnu.no>
2022-03-23 15:11:44 +0100

    Finished(?) project. See below: Error handling, clean up, prettier printing, added comments, the user get to see the coffetaste before submitting it.

--- main.py
+++ main.py
@@ -23,170 +23,159 @@ menu = """\n---- Valgmuligheter i programmet ----\n
 5. Vis kaffer fra Rwanda og Colombia som ikke er vaskede
 6. Avslutt programmet \n"""
 
+
 def main():
-   
+
     print(logoIntro)
     print(intro)
-    
+
     while True:
 
         print(menu)
-        tall = input("Skriv inn et tall mellom 1 og 6 for å velge hva du vil gjøre: ")
 
-        if(tall == "1"):
+        choice = input("Skriv inn et tall mellom 1 og 6 for å velge hva du vil gjøre: ")
+
+        if(choice == "1"):
             handleUserTasting()
 
-        elif(tall == "2"):
+        elif(choice == "2"):
             getListOverUsersWithMostTasted()
 
-        elif(tall == "3"):
+        elif(choice == "3"):
             getMostCoffePerPrice()
 
-        elif(tall == "4"):
-            keyword=input('Hvilket nøkkelord vil du søke etter? ')
+        elif(choice == "4"):
+            keyword = input('Hvilket nøkkelord vil du søke etter? ')
             getCoffeByKeyWord(keyword)
 
-        elif(tall == "5"):
+        elif(choice == "5"):
             getUnwashedCoffeeFromRwandaAndColombia()
-            
-        elif(tall == "6"):
+
+        elif(choice == "6"):
             exitProgram()
         else:
-            print("Ugyldig input! Input må være et tall mellom 1 og 6. \n")
-            
+            print("Ugyldig input! Input må være et tall mellom 1 og 6.")
+
         handleFollowUp()
 
 
 # input = 1
 def handleUserTasting():
-    # Henter først ut liste over alle tilgjengelige kaffer slik at brukeren kan velge hvilken hen vil skrive smaksnotat til 
-    print("Liste over tilgjengelige kaffer: \n")
-    query = ("""SELECT Kaffe.KaffeID, Kaffe.KaffeNavn, Kaffe.KaffeBeskrivelse, Kaffe.KiloprisNOK, Kaffebrenneri.BrenneriNavn, Kaffeboenne.Art, 
-    Kaffeboenne.Type, KaffeBrentAvBrenneri.Dato, Kaffegaard.GaardNavn, Kaffegaard.Region, 
-    Kaffegaard.Land, Kaffeparti.KiloprisUSD, Kaffeparti.Innhoestingsaar
-    FROM KAFFE
-        NATURAL JOIN KaffeBrentAvBrenneri
-        NATURAL JOIN Kaffebrenneri
-        NATURAL JOIN Kaffeparti
-        NATURAL JOIN Kaffegaard
-        NATURAL JOIN BestaarAv
-        NATURAL JOIN Kaffeboenne
-        NATURAL JOIN Foredlingsmetode """)
-    
-    # Så velger brukeren riktig notat fra tabellen
-    print(pd.read_sql_query(query, con))
+    # Henter først ut liste over alle tilgjengelige kaffer slik at brukeren kan velge hvilken hen vil skrive smaksnotat til.
+    # Har her fjernet Kaffe.KiloprisNOK, Kaffeboenne.Art, Kaffeboenne.Type for å spare plass og gi en bedre brukeropplevelse.
 
-    
-    cursor.execute("""SELECT Kaffe.KaffeID FROM KAFFE""")
+    print("Liste over tilgjengelige kaffer: \n")
+    query = ("""SELECT Kaffe.KaffeID, Kaffe.KaffeNavn, Kaffe.KaffeBeskrivelse, Kaffebrenneri.BrenneriNavn, KaffeBrentAvBrenneri.Dato, 
+    Kaffegaard.GaardNavn, Kaffegaard.Region, Kaffegaard.Land, Kaffeparti.KiloprisUSD, Kaffeparti.Innhoestingsaar
+                FROM KAFFE
+                    NATURAL JOIN KaffeBrentAvBrenneri
+                    NATURAL JOIN Kaffebrenneri
+                    NATURAL JOIN Kaffeparti
+                    NATURAL JOIN Kaffegaard
+                    NATURAL JOIN BestaarAv
+                    NATURAL JOIN Kaffeboenne
+                    NATURAL JOIN Foredlingsmetode """)
+
+    print(pd.read_sql_query(query, con))
+    print("")
+
+    # Henter ut kaffeID-ene som er i databasen og lagrer disse.
+    cursor.execute("""SELECT Kaffe.KaffeID 
+                      FROM KAFFE""")
+
     coffeeList = (cursor.fetchall())
+
+    # Så velger brukeren riktig notat fra tabellen og vi sjekker om kaffen har en gyldig ID
     validation = False
-    
-    # Her henter vi ut lister med kaffer og sjekker om kaffen har en gyldig ID
     while not validation:
-        coffeeId = int(input("Skriv inn ID til kaffen du vil smake på: "))
-        for i in coffeeList:
-            if (list(i)[0] == coffeeId): 
-                print('Good shit')
-                validation = True
-        if not validation:
-            print("IDen du valgte stemmer ikke overens med noen som finnes i databasen")
-    
-    #Tar datoen nå og formaterer den til en streng
+        try:
+            coffeeId = int(input("Skriv inn ID til kaffen du vil smake på: "))
+            for i in coffeeList:
+                if (list(i)[0] == coffeeId):
+                    validation = True
+            if not validation:
+                print(
+                    "IDen du valgte stemmer ikke overens med noen som finnes i databasen. Prøv igjen!")
+        except:
+            print("ID-en til en kaffe må være et tall!")
+
+    # Tar datoen nå og formaterer den til en streng
     now = datetime.now()
     date = now.strftime("%d.%m.%Y")
-    points = int(input("Hvor mange poeng vil du gi til kaffen? "))
-    while (points < 1 or points > 10):
-       points = int(input("Smakspoeng kan ikke være mindre enn 1 eller større enn 10. Prøv igjen: "))
-        # Håndter dette?
-    
-    tastenote = input("Fyll inn smaksnotat... ")
 
-    #Generer id til smaksnotatet
+    # Her håndterer vi at poengene som gis er gyldige poeng
+    validation = False
+    while not validation:
+        try:
+            points = int(input("Hvor mange poeng vil du gi til kaffen? "))
+            while (points < 1 or points > 10):
+                points = int(
+                    input("Smakspoeng kan ikke være mindre enn 1 eller større enn 10. Prøv igjen: "))
+            validation = True
+        except:
+            print("Poeng må være på tallformat! Prøv igjen.")
+
+    # Smaksnotatet lagres
+    tastenote = input("Fyll inn smaksnotat... ")
+    print("")
+
+    # Generer id til smaksnotatet
     cursor.execute("""SELECT Kaffesmaking.NotatID FROM Kaffesmaking""")
     idlist = (cursor.fetchall())
     id = len(idlist) + 1
 
-    # Insert til databasen skjer her:
+    # Insert til databasen skjer her. Epostadressen vi lagrer notatet under eksisterer allerede i databasen:
     cursor.execute("""INSERT INTO Kaffesmaking VALUES (:ID, :tastenote, :points, :date, :coffeeId, "bruker@historie1.no")""",
-     {"ID": (id), "tastenote": (tastenote), "points": (points), "date": (date), "coffeeId": (coffeeId)} )
-    con.commit()
-    
-    
+                   {"ID": (id), "tastenote": (tastenote), "points": (points), "date": (date), "coffeeId": (coffeeId)})
+
+    print("-"*30)
+    print("Smaksnotat: " + tastenote)
+    print("Smakspoeng: " + str(points))
+    print("Smakdato: " + date)
+    print("KaffeID valgt: " + str(coffeeId))
+    print("-"*30 + "\n")
+
+    # Lar brukeren få dobbeltsjekke at kaffesmakingen er det man ønsket
+    choice = input(
+        "Vil du lagre kaffesmakingen til databasen? Skriv 'J' eller 'j' for å lagre eller hva som helst for å avbryte: ")
+    if(choice == "J" or choice == "j"):
+        try:
+            con.commit()
+            print("Kaffesmakingen ble lagret suksessfullt!")
+        except:
+            print("Kaffesmakingen kunne ikke lagres. Prøv igjen!")
+    else:
+        print("Kaffesmakingen ble ikke lagret.")
+
 
 # input = 2
 def getListOverUsersWithMostTasted():
     query = ("""SELECT Bruker.Fornavn, Bruker.Etternavn, count (distinct KaffeID) as antallUnikeKafferSmakt
-                    FROM  Kaffesmaking NATURAL JOIN Bruker
-                    WHERE Smaksdato LIKE '%2022'
-                    GROUP BY Kaffesmaking.Epostadresse
-                    ORDER BY antallUnikeKafferSmakt DESC
-                    """)
-
-    cursor.execute("""SELECT Bruker.Fornavn, Bruker.Etternavn, count (distinct KaffeID) as antallUnikeKafferSmakt
-                    FROM  Kaffesmaking NATURAL JOIN Bruker
-                    WHERE Smaksdato LIKE '%2022'
-                    GROUP BY Kaffesmaking.Epostadresse
-                    ORDER BY antallUnikeKafferSmakt DESC
-                    """)
-    row=cursor.fetchall()
+                FROM  Kaffesmaking NATURAL JOIN Bruker
+                WHERE Smaksdato LIKE '%2022'
+                GROUP BY Kaffesmaking.Epostadresse
+                ORDER BY antallUnikeKafferSmakt DESC""")
     print("")
     print("Resultatet ble: \n")
     print(pd.read_sql_query(query, con))
-   # print(row)  # denne gir styggere output
 
-# input = 3 
+# input = 3
 def getMostCoffePerPrice():
     query = ("""SELECT KaffeNavn, avg(Poeng) as SnittPoeng, KiloprisNOK, BrenneriNavn
-                    FROM Kaffesmaking
+                FROM Kaffesmaking
                     NATURAL JOIN Kaffe
                     NATURAL JOIN KaffeBrentAvBrenneri
                     NATURAL JOIN Kaffebrenneri
-                    GROUP BY kaffeID
-                    ORDER BY (Snittpoeng/KiloprisNOK) DESC
-                    """)
-    
-    cursor.execute("""SELECT KaffeNavn, avg(Poeng) as SnittPoeng, KiloprisNOK, BrenneriNavn
-                    FROM Kaffesmaking
-                    NATURAL JOIN Kaffe
-                    NATURAL JOIN KaffeBrentAvBrenneri
-                    NATURAL JOIN Kaffebrenneri
-                    GROUP BY kaffeID
-                    ORDER BY (Snittpoeng/KiloprisNOK) DESC
-                    """)
+                GROUP BY kaffeID
+                ORDER BY (Snittpoeng/KiloprisNOK) DESC""")
+
     print("")
     print("Resultatet ble: \n")
     print(pd.read_sql_query(query, con))
-
-    #Hjemmesnekra print:
-    """
-    row=cursor.fetchall()
-    i=1
-    for entry in row:
-        print(str(i) + ".", entry)
-        i=i + 1 """
-
 
 # input = 4
 def getCoffeByKeyWord(keyword):
-    keyword="%" + keyword + "%"
-    print(keyword)
-    query = ("""SELECT BrenneriNavn, KaffeNavn
-                        FROM Kaffe
-                            NATURAL JOIN KaffeBrentAvBrenneri
-                            NATURAL JOIN Kaffebrenneri
-                        WHERE Kaffe.KaffeBeskrivelse LIKE :keyword
-
-                        UNION
-
-                        SELECT BrenneriNavn, KaffeNavn
-                        FROM Kaffesmaking
-                            NATURAL JOIN Kaffe
-                            NATURAL JOIN KaffeBrentAvBrenneri
-                            NATURAL JOIN Kaffebrenneri
-                        WHERE Kaffesmaking.Smaksnotat LIKE :keyword
-
-                    """, keyword)
-
+    keyword = "%" + keyword + "%"
 
     cursor.execute("""SELECT BrenneriNavn, KaffeNavn
                         FROM Kaffe
@@ -205,22 +194,20 @@ def getCoffeByKeyWord(keyword):
 
                     """, {"keyword": (keyword)})
 
-
-    row=cursor.fetchall()
-
+    row = cursor.fetchall()
     if len(row) == 0:
-        print("Det var ingen kaffer som stemte med søket ditt etter '" + keyword[1:-1] + "'")
+        print("Det var ingen kaffer som stemte med søket ditt etter '" +
+              keyword[1:-1] + "'")
     else:
+        # Hadde problemer med at Pandas ikke likte SQL-spørringer hvor vi har variabler, i dette tilfellet "keyword" 
+        # som input fra brukeren. Måtte derfor bruker cursor.execute og lage en hjemmelagd "finere" tabell. Håper det er OK!
         print("")
         print("Resultatet ble: \n")
-        #print(pd.read_sql_query(query, con))
-
         print("BrenneriNavn".rjust(19) + "KaffeNavn".rjust(30))
-        i=0
+        i = 0
         for entry in row:
             print(str(i) + entry[0].rjust(18) + " " + entry[1].rjust(29))
-            i=i + 1
-        #print(row)
+            i += 1
 
 #input = 5
 def getUnwashedCoffeeFromRwandaAndColombia():
@@ -248,26 +235,28 @@ def getUnwashedCoffeeFromRwandaAndColombia():
         NATURAL JOIN Foredlingsmetode
     WHERE (Kaffegaard.Land LIKE "Colombia" OR Kaffegaard.Land LIKE "Rwanda") AND Foredlingsmetode.MetodeNavn LIKE "Vasket"
         """)
-    
+
     print(pd.read_sql_query(query, con))
 
 
 # input = 6
+# Stenger tilkoblingen og avslutter programmet. 
 def exitProgram():
     print("\n---- Takk for nå! ----\n")
     con.close()
     quit()
 
+# Håndtere om brukeren vil sjekke ut de andre valgmuligheter
 def handleFollowUp():
-    choice = str(input("\nTast 'J' for å se menyen på nytt eller 'N' for å avslutte programmet: "))
+    choice = str(input(
+        "\nTast 'J' for å se menyen på nytt eller 'N' for å avslutte programmet: "))
     while True:
-        if(choice == "J" or choice == "j" ):
-           return True
+        if(choice == "J" or choice == "j"):
+            return True
         elif(choice == 'N' or choice == "n"):
             exitProgram()
         else:
-            choice = str(input("Ugyldig input! Tast 'J' for å se menyen på nytt eller 'N' for å avslutte programmet: "))
+            choice = str(input(
+                "Ugyldig input! Tast 'J' eller 'j' for å se menyen på nytt eller 'N' eller 'n' for å avslutte programmet: "))
 
-
-#main()
-handleUserTasting()
+main()
