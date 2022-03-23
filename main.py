296a5b1c6e16471492778c890552163038e24c37
matskl <matskl@stud.ntnu.no>
2022-03-23 13:46:36 +0100

    Almost finished with user story 1 and 5
    Co-authored-by: Sebastian Hegreberg <sebasheg@stud.ntnu.no>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+from datetime import datetime
 
 con = sqlite3.connect("kaffe.db")
 cursor = con.cursor()
@@ -33,7 +34,7 @@ def main():
         tall = input("Skriv inn et tall mellom 1 og 6 for å velge hva du vil gjøre: ")
 
         if(tall == "1"):
-            print("Funksjon 1 kommer her \n")
+            handleUserTasting()
 
         elif(tall == "2"):
             getListOverUsersWithMostTasted()
@@ -46,7 +47,7 @@ def main():
             getCoffeByKeyWord(keyword)
 
         elif(tall == "5"):
-            print("Funksjon 5 kommer her")
+            getUnwashedCoffeeFromRwandaAndColombia()
             
         elif(tall == "6"):
             exitProgram()
@@ -54,6 +55,63 @@ def main():
             print("Ugyldig input! Input må være et tall mellom 1 og 6. \n")
             
         handleFollowUp()
+
+
+# input = 1
+def handleUserTasting():
+    # Henter først ut liste over alle tilgjengelige kaffer slik at brukeren kan velge hvilken hen vil skrive smaksnotat til 
+    print("Liste over tilgjengelige kaffer: \n")
+    query = ("""SELECT Kaffe.KaffeID, Kaffe.KaffeNavn, Kaffe.KaffeBeskrivelse, Kaffe.KiloprisNOK, Kaffebrenneri.BrenneriNavn, Kaffeboenne.Art, 
+    Kaffeboenne.Type, KaffeBrentAvBrenneri.Dato, Kaffegaard.GaardNavn, Kaffegaard.Region, 
+    Kaffegaard.Land, Kaffeparti.KiloprisUSD, Kaffeparti.Innhoestingsaar
+    FROM KAFFE
+        NATURAL JOIN KaffeBrentAvBrenneri
+        NATURAL JOIN Kaffebrenneri
+        NATURAL JOIN Kaffeparti
+        NATURAL JOIN Kaffegaard
+        NATURAL JOIN BestaarAv
+        NATURAL JOIN Kaffeboenne
+        NATURAL JOIN Foredlingsmetode """)
+    
+    # Så velger brukeren riktig notat fra tabellen
+    print(pd.read_sql_query(query, con))
+
+    
+    cursor.execute("""SELECT Kaffe.KaffeID FROM KAFFE""")
+    coffeeList = (cursor.fetchall())
+    validation = False
+    
+    # Her henter vi ut lister med kaffer og sjekker om kaffen har en gyldig ID
+    while not validation:
+        coffeeId = int(input("Skriv inn ID til kaffen du vil smake på: "))
+        for i in coffeeList:
+            if (list(i)[0] == coffeeId): 
+                print('Good shit')
+                validation = True
+        if not validation:
+            print("IDen du valgte stemmer ikke overens med noen som finnes i databasen")
+    
+    #Tar datoen nå og formaterer den til en streng
+    now = datetime.now()
+    date = now.strftime("%d.%m.%Y")
+    points = int(input("Hvor mange poeng vil du gi til kaffen? "))
+    while (points < 1 or points > 10):
+       points = int(input("Smakspoeng kan ikke være mindre enn 1 eller større enn 10. Prøv igjen: "))
+        # Håndter dette?
+    
+    tastenote = input("Fyll inn smaksnotat... ")
+
+    #Generer id til smaksnotatet
+    cursor.execute("""SELECT Kaffesmaking.NotatID FROM Kaffesmaking""")
+    idlist = (cursor.fetchall())
+    id = len(idlist) + 1
+
+    # Insert til databasen skjer her:
+    cursor.execute("""INSERT INTO Kaffesmaking VALUES (:ID, :tastenote, :points, :date, :coffeeId, "bruker@historie1.no")""",
+     {"ID": (id), "tastenote": (tastenote), "points": (points), "date": (date), "coffeeId": (coffeeId)} )
+    con.commit()
+    
+    
 
 # input = 2
 def getListOverUsersWithMostTasted():
@@ -165,7 +223,33 @@ def getCoffeByKeyWord(keyword):
         #print(row)
 
 #input = 5
-# def ....
+def getUnwashedCoffeeFromRwandaAndColombia():
+    query = ("""SELECT  Kaffebrenneri.BrenneriNavn, Kaffe.KaffeNavn
+	FROM Kaffe
+	NATURAL JOIN KaffeBrentAvBrenneri
+	NATURAL JOIN Kaffebrenneri
+	NATURAL JOIN Kaffeparti
+	NATURAL JOIN Kaffegaard
+	NATURAL JOIN DyrkesAv
+	NATURAL JOIN Kaffeboenne
+	NATURAL JOIN Foredlingsmetode 
+    WHERE Kaffegaard.Land LIKE "Colombia" OR Kaffegaard.Land LIKE "Rwanda"
+
+    EXCEPT
+
+    SELECT  Kaffebrenneri.BrenneriNavn, Kaffe.KaffeNavn
+    FROM Kaffe
+        NATURAL JOIN KaffeBrentAvBrenneri
+        NATURAL JOIN Kaffebrenneri
+        NATURAL JOIN Kaffeparti
+        NATURAL JOIN Kaffegaard
+        NATURAL JOIN DyrkesAv
+        NATURAL JOIN Kaffeboenne
+        NATURAL JOIN Foredlingsmetode
+    WHERE (Kaffegaard.Land LIKE "Colombia" OR Kaffegaard.Land LIKE "Rwanda") AND Foredlingsmetode.MetodeNavn LIKE "Vasket"
+        """)
+    
+    print(pd.read_sql_query(query, con))
 
 
 # input = 6
@@ -185,4 +269,5 @@ def handleFollowUp():
             choice = str(input("Ugyldig input! Tast 'J' for å se menyen på nytt eller 'N' for å avslutte programmet: "))
 
 
-main()
+#main()
+handleUserTasting()
